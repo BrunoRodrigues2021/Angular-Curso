83f48c913bc6a46d9a5095c422f030eb08ebb51c
Bruno <bruno.rodrigues66@fatec.sp.gov.br>
2021-09-09 11:38:30 -0300

    Aula: Diretivas - *ngFor

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
-import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
